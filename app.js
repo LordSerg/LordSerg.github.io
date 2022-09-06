8db3f0b42e48a7c949329ac7bf5869bebb8fa4f8
LordSerg <oknelaksoms@gmail.com>
2022-09-06 22:43:07 +0300

    blog and contact sections and day/night mode

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ function PageTransitions()
             element.classList.add('active');
         }
     });
+
+    //change theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>
+    {
+        let element=document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
